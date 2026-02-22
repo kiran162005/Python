cffdbb63ee10686c95fa615730c3bd9717cc9d3f
kiran162005 <Kirantraj16@gmail.com>
2026-02-22 18:58:11 +0530

    Temperature Conversion

--- 8_temp-conversion.py
+++ 8_temp-conversion.py
@@ -1,3 +1,7 @@
 unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
 temp = float(input("Enter the temperature: "))
 
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temperature in Fahrenheit is: {temp}°F")
+
