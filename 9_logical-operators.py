175c5707ad420849573375d587640c48c0bdda33
kiran162005 <Kirantraj16@gmail.com>
2026-02-23 22:36:19 +0530

    Logical Operators

---
+++ 9_logical-operators.py
@@ -0,0 +1,6 @@
+# logical operators = evaluate multiple conditions(or, and, not)
+
+# or = at least one condition must be True
+# and = both the condition must be True
+# not = inverts the condition (not True, not False)
+
