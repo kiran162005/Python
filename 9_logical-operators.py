e5a0bab547ccba80a2bdbac254899de6c57e0b8e
kiran162005 <Kirantraj16@gmail.com>
2026-02-23 22:39:02 +0530

    Logical Operators

--- 9_logical-operators.py
+++ 9_logical-operators.py
@@ -29,3 +29,6 @@ elif 28 > temp > 0 and is_sunny:
 elif temp >= 28  and not is_sunny:
     print("It is HOT outside")
     print("It is CLOUDY")
+elif temp <= 0 and not is_sunny:
+    print("It is COLD outside")
+    print("It is CLOUDY")
