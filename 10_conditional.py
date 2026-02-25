e4f055f02c658bb8acd8b33151c146c8d26869cd
kiran162005 <Kirantraj16@gmail.com>
2026-02-25 21:06:08 +0530

    Conditional expression

--- 10_conditional.py
+++ 10_conditional.py
@@ -11,3 +11,12 @@ age = 25
 temp = 20
 user_role = "admin"
 
+# print("Positive" if num > 0 else "negative")
+# result = "Even" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temp > 20 else "COLD"
+access_level = "FULL Access" if user_role == "admin" else "Limited Access"
+
+print(access_level)
