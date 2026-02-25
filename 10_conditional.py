57440574650ab147233316264a3311a2b3321b44
kiran162005 <Kirantraj16@gmail.com>
2026-02-25 21:05:12 +0530

    Conditional expression

---
+++ 10_conditional.py
@@ -0,0 +1,21 @@
+# conditional expression = A onr line shortcut for the if-else statement(ternary operator)
+#                          Print or assign one of two values based on a condition
+#                          X if condition else Y
+
+
+num = 5
+a= 6
+b = 7
+age = 25
+temp = 20
+user_role = "admin"
+
+# print("Positive" if num > 0 else "negative")
+# result = "Even" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temp > 20 else "COLD"
+access_level = "FULL Access" if user_role == "admin" else "Limited Access"
+
+print(access_level)
