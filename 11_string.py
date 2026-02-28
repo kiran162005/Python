fb5ddb95c9c6179c905b9caa14b9765377fcf71e
kiran162005 <Kirantraj16@gmail.com>
2026-02-28 17:55:07 +0530

    Strings

--- 11_string.py
+++ 11_string.py
@@ -29,3 +29,18 @@ print(phone)
 # 3. username must not contain digits
 
 
+'''
+username = input("Enter a username: ")
+
+
+if len(username) > 12:
+    print("Username can't be more than 12 characters")
+elif not username.find(" ") == 1:
+    print("Your username cant contain spaces")
+elif not username.isalpha():
+    print("Your username cant contain numbersk")
+else:
+    print(f"Welcome {username}")
+'''
+
+
