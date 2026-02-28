f2a4617e5f57aba14c0edca05d51cc655682552b
kiran162005 <Kirantraj16@gmail.com>
2026-02-28 17:54:10 +0530

    Strings

--- 11_string.py
+++ 11_string.py
@@ -2,3 +2,10 @@
 
 # result = len(name)
 # print(name.find("r"))
+# print(name.rfind("r"))
+# name = name.capatalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+
