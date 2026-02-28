abf43c81fd6924f1eb13f564d6b7444e0d7593a9
kiran162005 <Kirantraj16@gmail.com>
2026-02-28 17:55:19 +0530

    Strings

--- 11_string.py
+++ 11_string.py
@@ -44,3 +44,17 @@ else:
 '''
 
 
+#indexing = accessing elements of a sequence using [] (indexing operator)
+            # [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[4])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-3])
+# print(credit_number[::3])
+
+last_digits = credit_number[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
