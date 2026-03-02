adf25cb1622034ec2dcede49456a7147c0c3b14b
kiran162005 <Kirantraj16@gmail.com>
2026-03-02 21:38:10 +0530

    While Loop

--- 13_whileloop.py
+++ 13_whileloop.py
@@ -8,3 +8,9 @@ while name == "":
 print(f"Hello {name}")"""
 
 
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age cant be negative")
+    age = int(input("Enter your age: "))
+print(f"You are {age} years old")
