25f15773413bc130debe5935b97f5ed057d8de84
kiran162005 <Kirantraj16@gmail.com>
2026-03-04 22:25:46 +0530

    Compund Interest Calculator

---
+++ 14_compond_interest.py
@@ -0,0 +1,30 @@
+#python copound interest calculator
+
+principle = 0
+rate = 0
+time = 0
+
+while True:
+    principle = float(input("Enter the principle amount:"))
+    if principle <= 0:
+        print("Principle cant be less than or equal to zero")
+    else:
+        break
+
+while True:
+    rate = float(input("Enter the interest rate:"))
+    if rate <= 0:
+        print("Interest rate cant be less than or equal to zero")
+    else:
+        break
+
+while time <= 0:
+    time = int(input("Enter the time in years:"))
+    if time <= 0:
+        print("Time cant be less than or equal to zero")
+
+total = principle * pow((1+rate / 100), time)
+print(f"Balance after {time} year/s: ${total:.2f}")
+print(principle)
+print(rate)
+print(time)
