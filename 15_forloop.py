882caf1e4b6a2afe40779d14c4b9cc1d48852fd3
kiran162005 <Kirantraj16@gmail.com>
2026-03-05 22:02:51 +0530

    For loop

---
+++ 15_forloop.py
@@ -0,0 +1,27 @@
+#for loops = execute a blockc of code a fixed number of times.
+# You can iterate over a range, string, sequence, etc
+
+# for x in range(1, 11):
+#     print(x)
+
+
+'''for a in reversed(range(1, 11)):
+    print(a)
+print("HAPPY BIRTHDAY! ")
+'''
+
+# for x in range(1, 11, 3):
+#     print(x)
+
+
+
+'''credit_card = "1234-5678-9012-3456"
+for x in credit_card:
+    print(x)
+'''
+
+for x in range(1,21):
+    if x == 13:
+        break
+    else:
+        print(x)
