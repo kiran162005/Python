f6a1d87f8e27af5111454e2196898ea9b15c508b
kiran162005 <Kirantraj16@gmail.com>
2026-03-09 21:18:18 +0530

    Shopping cart

--- 19_shopping-cart.py
+++ 19_shopping-cart.py
@@ -6,4 +6,10 @@ total = 0
 
 while True:
     food = input("Enter a food to buy(q to quit): ")
-    
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of {food}: "))
+        foods.append(food)
+        prices.append(price)
+
