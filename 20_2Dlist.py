04a8d1cf7346aabdbc8099e1c4a8624fabca99d1
kiran162005 <Kirantraj16@gmail.com>
2026-03-11 20:51:17 +0530

    2D Lists

--- 20_2Dlist.py
+++ 20_2Dlist.py
@@ -18,3 +18,12 @@ for collection in groceries:
     print()
 '''
 
+num_pad = ((1,2,3),
+           (4,5,6),
+           (7,8,9),
+           ("*", 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end= " ")
+    print()
