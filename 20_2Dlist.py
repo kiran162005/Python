da67f9f8fa2573801414ec5e52d35233e3598eb4
kiran162005 <Kirantraj16@gmail.com>
2026-03-11 20:51:04 +0530

    2D Lists

--- 20_2Dlist.py
+++ 20_2Dlist.py
@@ -7,3 +7,14 @@
 # print(groceries[1][1])
 
 
+'''
+groceries = [["apple", "orange", "banaga", "coconut"],
+            ["carrots", "potatoes", "beans"],
+            ["chicken", "fish", "turkey"]]
+
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
+    print()
+'''
+
