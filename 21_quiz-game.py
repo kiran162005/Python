92102102d2215991ee5e0ae07b5a745abc862408
kiran162005 <Kirantraj16@gmail.com>
2026-03-13 22:39:32 +0530

    Quiz game

--- 21_quiz-game.py
+++ 21_quiz-game.py
@@ -18,3 +18,20 @@ score = 0
 question_num = 0
 
 
+for question in questions:
+    print("------------------------------------------------------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
+
+
