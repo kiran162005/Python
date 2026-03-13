8156f57cab9ff55c80e6a6c61f70988bc9bc6ae0
kiran162005 <Kirantraj16@gmail.com>
2026-03-13 22:38:50 +0530

    Quiz game

--- 21_quiz-game.py
+++ 21_quiz-game.py
@@ -6,3 +6,9 @@ questions = ("How many elements are in the periodic table?: ",
              "How many bones are in humnn body?: ",
              "Which planet in the solar system is the hottes?: ")
 
+options = (("A. 116", "B. 117 ", "C. 118 ", "D. 119 "), 
+           ("A. Whale", "B. crocodile", "C. Elephant", "D. ostrich "), 
+           ("A. Nitrogen", "B. Oxygen", "C. carbon-Dioxide", "D. Hydrogen "), 
+           ("A. 206", "B. 207 ", "C. 208", "D. 209"), 
+           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+
