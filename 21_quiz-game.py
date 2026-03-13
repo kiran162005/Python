34b84da51e4a2cef6375e69bb6afe5264a1521f9
kiran162005 <Kirantraj16@gmail.com>
2026-03-13 22:38:05 +0530

    Quiz game

---
+++ 21_quiz-game.py
@@ -0,0 +1,8 @@
+#Python quiz game
+
+questions = ("How many elements are in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are in humnn body?: ",
+             "Which planet in the solar system is the hottes?: ")
+
