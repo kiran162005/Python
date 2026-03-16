c4a1369d8375eed3244c046b0b0319ad25553da9
kiran162005 <Kirantraj16@gmail.com>
2026-03-16 23:06:55 +0530

    Concession Stand program

--- 23_concession-stand.py
+++ 23_concession-stand.py
@@ -18,3 +18,10 @@ for key, value in menu.items():
     print(f"{key:10}: {value: .2f}")
 print("-----------------------")
 
+while True:
+    food = input("Select an item (q to quit): ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
