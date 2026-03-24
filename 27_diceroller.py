e1c704c21af540176994632aacb0659457b71b08
kiran162005 <Kirantraj16@gmail.com>
2026-03-24 22:21:02 +0530

    Dice roller

--- 27_diceroller.py
+++ 27_diceroller.py
@@ -58,3 +58,7 @@ num_of_dice = int(input("How many dice? "))
 for die in range(num_of_dice):
     dice.append(random.randint(1,6))
 
+# for die in range(num_of_dice):
+#     for line in dice_art.get(dice[die]):
+#         print(line)
+
