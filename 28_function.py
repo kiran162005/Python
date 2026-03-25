36597e08cec9bfece0a5f7354ca310099b05bd7a
kiran162005 <Kirantraj16@gmail.com>
2026-03-25 21:38:22 +0530

    function

--- 28_function.py
+++ 28_function.py
@@ -20,3 +20,20 @@ display_invoice("Alice", 43.20, "01/01")
 # return = statemenr used to end a function
 #           and send a result back to the caller
 
+def add(x, y):
+    z = x + y
+    return z
+
+def sub(x, y):
+    z = x - y
+    return z
+
+def mul(x, y):
+    z = x * y
+    return z
+
+def div(x, y):
+    z = x / y
+    return z
+
+
