a46f5ce3703e8de7430db39dfe27d3fb56bf3c8e
kiran162005 <Kirantraj16@gmail.com>
2026-03-28 22:57:36 +0530

    Argunemts

--- 29_arguments.py
+++ 29_arguments.py
@@ -16,3 +16,14 @@
 # # print(net_price(500))
 # print(net_price(500,0.1))
 
+'''
+import time
+
+def count(end,start=0):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("DONE!")
+
+count(30, 15)
+'''
