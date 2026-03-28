ad6f610e0f822eba834d7668c1506d6e7495f381
kiran162005 <Kirantraj16@gmail.com>
2026-03-28 22:58:14 +0530

    Argunemts

--- 29_arguments.py
+++ 29_arguments.py
@@ -31,3 +31,6 @@ count(30, 15)
 
 # keyword arguments = an argument preceded by an identifier
 # helps with readability
+# order of arguments doesn't matter
+# 1. positional 2. default 3. KEYWORD 4. arbitrary
+
