de606757dd6cf84747a16114306e0716c4265c8d
kiran162005 <Kirantraj16@gmail.com>
2026-03-28 22:58:41 +0530

    Argunemts

--- 29_arguments.py
+++ 29_arguments.py
@@ -34,3 +34,8 @@ count(30, 15)
 # order of arguments doesn't matter
 # 1. positional 2. default 3. KEYWORD 4. arbitrary
 
+'''def hello(greeting, title, first, last):
+    print(f"{greeting} {title}{first} {last}")
+
+hello("Hello", title="Mr.", last="Squarepants", first="Spongebob")
+'''
