cdff77cb9f02613b0e885037fbdd232e8f23f8bb
kiran162005 <Kirantraj16@gmail.com>
2026-03-28 22:59:08 +0530

    Argunemts

--- 29_arguments.py
+++ 29_arguments.py
@@ -42,3 +42,9 @@ hello("Hello", title="Mr.", last="Squarepants", first="Spongebob")
 
 # for x in range(1,11):
 #     print(x, end=" ")
+
+def get_phone(country, area, first, last):
+    return f"{country}-{area}-{first}-{last}"
+
+phone_num = get_phone(country=91, area=123, first=456, last=7890)
+print(phone_num)
