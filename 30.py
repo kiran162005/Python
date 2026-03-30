98e2790e55a8a637683f60740462b6e920eed8c4
kiran162005 <Kirantraj16@gmail.com>
2026-03-30 21:54:44 +0530

    args, kwargs

--- 30.py
+++ 30.py
@@ -34,3 +34,9 @@ def shipping_label(*args, **kwargs):
         print(arg, end=" ")
     print()
 
+
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apt')}")
+    else:
+        print(f"{kwargs.get('street')} ")
+
