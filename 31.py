7ac0d3d2ed2d135966c09cbf2e5d7044937e1f55
kiran162005 <Kirantraj16@gmail.com>
2026-04-04 23:30:51 +0530

    Iterables

--- 31.py
+++ 31.py
@@ -3,21 +3,3 @@
 
 # numbers = [1, 2, 3, 4, 5]
 
-# # for number in numbers:
-# #     print(number)
-
-# for number in reversed(numbers):
-#     print(number, end=" ")
-
-
-# fruits = {"apple", "orange", "banana", "coconut"}  # Sets cant be reversed
-# for fruit in fruits:
-#     print(fruit)
-
-# name = "Kiran Raj"
-# for character in name:
-#     print(character, end=" ")
-
-my_dictionary = {"A": 1, "B": 2, "C": 3}
-for key,value in my_dictionary.items():
-    print(f"{key} = {value}")
