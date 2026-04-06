c29aad8adfd30022b75a29b958bac2e2a2551beb
kiran162005 <Kirantraj16@gmail.com>
2026-04-06 21:50:40 +0530

    Membership operators

--- 32.py
+++ 32.py
@@ -16,3 +16,8 @@ else:
 # students = {"Spongebob", "Patrick", "Sandy"}
 # student = input("Enter the name of a student: ")
 
+# if student in students:
+#     print(f"{student} is a student")
+# else:
+#     print(f"{student} was not found")
+
