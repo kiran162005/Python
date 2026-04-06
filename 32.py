617ea78c86e8cb11a38a8f367de0775773734e83
kiran162005 <Kirantraj16@gmail.com>
2026-04-06 21:49:51 +0530

    Membership operators

--- 32.py
+++ 32.py
@@ -7,3 +7,9 @@
 word = "APPLE"
 
 letter = input("Guess a letter in the secret word: ")
+if letter not in word:
+    print(f"{letter} was not found")
+else:
+    print(f"There is a {letter}")
+'''
+
