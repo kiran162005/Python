db574005681e02f75a15b44ed4b901bc5b2a334e
kiran162005 <Kirantraj16@gmail.com>
2026-04-06 21:51:50 +0530

    Membership operators

--- 32.py
+++ 32.py
@@ -31,3 +31,8 @@ grades = {
 
 student = input("Enter the name of a student: ")
 
+if student in grades:
+    print(f"{student}'s grade is {grades[student]}")
+else:
+    print(f"{student} was not found")
+'''
