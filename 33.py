3d11ee31374b694522d1e002facac5461e2ec3b9
kiran162005 <Kirantraj16@gmail.com>
2026-04-07 22:04:10 +0530

    List comprehension

--- 33.py
+++ 33.py
@@ -34,3 +34,9 @@ positive_nums = [num for num in numbers if num >= 0]
 negative_nums = [num for num in numbers if num < 0]
 even_nums = [num for num in numbers if num % 2 ==0]
 odd_nums = [num for num in numbers if num % 2 ==1]
+print(odd_nums)
+print(even_nums)
+print(negative_nums)
+print(positive_nums)
+'''
+
