3e36606131a749b4dae9f6cc95e9f6e57010d698
kiran162005 <Kirantraj16@gmail.com>
2026-04-07 22:02:26 +0530

    List comprehension

--- 33.py
+++ 33.py
@@ -9,37 +9,3 @@ for x in range(1, 11):
 
 print(doubles)
 '''
-
-'''
-doubles = [x * 2 for x in range(1,11)]
-triples = [y * 3 for y in range(1,11)]
-squares = [z ** 2 for z in range(1,11)]
-
-print(doubles)
-print(triples)
-print(squares)
-'''
-
-'''
-fruits = ["apple", "orange", "banana", "coconut"]
-fruit_chars = [fruit[0] for fruit in fruits]
-fruits = [fruit.upper() for fruit in fruits]
-print(fruit_chars)
-print(fruits)
-'''
-
-'''
-numbers = [1, -2, 3, -4, 5, -6]
-positive_nums = [num for num in numbers if num >= 0]
-negative_nums = [num for num in numbers if num < 0]
-even_nums = [num for num in numbers if num % 2 ==0]
-odd_nums = [num for num in numbers if num % 2 ==1]
-print(odd_nums)
-print(even_nums)
-print(negative_nums)
-print(positive_nums)
-'''
-
-grades = [85, 42, 79, 90, 56, 61, 30]
-passing_grades = [grade for grade in grades if grade >= 60]
-print(passing_grades)
