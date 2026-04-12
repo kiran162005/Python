5667c90a36c08dcc0fd56370c9dc1e9d20924003
kiran162005 <Kirantraj16@gmail.com>
2026-04-12 23:28:18 +0530

    Match-case statement

--- 34.py
+++ 34.py
@@ -3,3 +3,9 @@
 # Benefits: cleaner and syntax is more readable
 
 def day_of_week(day):
+    match day:
+        case 1:
+            return "It is Sunday"
+        case 2:
+            return "It is Monday"
+        
