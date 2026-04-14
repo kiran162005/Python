a1564f9363dd5c8ef00db735dde6a6112abfaed6
kiran162005 <Kirantraj16@gmail.com>
2026-04-14 22:16:45 +0530

    Module

---
+++ 35.py
@@ -0,0 +1,25 @@
+# module = a file containing code you want to include in your program
+# use 'import' to include a module (built-in or your own)
+# useful to break up a large program reusable separate files
+
+# print(help("modules"))
+# print(help("math"))
+
+
+# import math
+# import math as m
+# print(m.pi)
+
+# from math import pi
+# print(pi)
+
+# from math import e
+# print(e)
+
+from math import e
+a, b, c, d, e = 1, 2, 3, 4, 5
+print(e ** a)
+print(e ** b)
+print(e ** c)
+print(e ** d)
+print(e ** e)
