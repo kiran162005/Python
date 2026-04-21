06070530a3bd806fd6958087c1f81e0703b198c9
kiran162005 <Kirantraj16@gmail.com>
2026-04-21 23:18:02 +0530

    Python

--- 38.py
+++ 38.py
@@ -1,13 +1,3 @@
 # if __name__ == '__main__
 # 
 # if __name__ == '__main__: (this script can be imported OR run standalone)
-# Functions and classes in this module can be reused
-# without the main block of code executing
-
-def main():
-# Your program goes here
-    print("Hii")
-
-
-if __name__ == '__main__':
-    main()
