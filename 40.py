7eca222300a6f9d321c4b778f4c8d78a10fd9cd9
kiran162005 <Kirantraj16@gmail.com>
2026-04-25 23:24:14 +0530

    Python Slot machine

--- 40.py
+++ 40.py
@@ -6,3 +6,9 @@ def spin_row():
     symbols = ['🍒', '🍉', '🍋', '🔔', '⭐']
     return [random.choice(symbols) for _ in range(3)]
 
+    # results = []
+    # for symbol in range(3):
+    #     results.append(random.choice(symbols))
+    # return results
+
+
