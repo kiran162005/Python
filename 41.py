728029be6c4ea26116f1d3a97abcd27c853d6b5c
kiran162005 <Kirantraj16@gmail.com>
2026-04-26 23:17:56 +0530

    Encription program

--- 41.py
+++ 41.py
@@ -16,3 +16,7 @@ random.shuffle(key)
 plain_text = input("Enter a message to encrypt: ")
 cipher_text = ""
 
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
