70fe4b81a29de8e050103df2b73322f21b470b35
kiran162005 <Kirantraj16@gmail.com>
2026-05-02 23:39:04 +0530

    Object

--- 43.py
+++ 43.py
@@ -1,25 +1,2 @@
 # object = A bundle of related attributes (variables) and methods (functions)
 # Example: Phone, Cup, Book
-# You need a class to create many objects 
-
-# class = (blueprint) used to design the structure and layout of an object 
-
-from car import Car
-
-
-car1 = Car("Mustang", 2024, "red", False)
-car2 = Car("Corvette", 2025, "blue", True)
-
-# print(car1.model)
-# print(car1.year)
-# print(car1.color)
-# print(car1.for_sale)
-
-print(car2.model)
-print(car2.year)
-print(car2.color)
-print(car2.for_sale)
-
-car2.describe()
-car2.drive()
-car2.stop()
