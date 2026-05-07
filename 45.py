c968ac68349088d293d537eb4a0507b359c1bb4a
kiran162005 <Kirantraj16@gmail.com>
2026-05-07 23:25:19 +0530

    Inheritance

---
+++ 45.py
@@ -0,0 +1,49 @@
+# Inheritance = Allows a class to inherit attributes and methods from another class
+# Helps with code reusability and extensibility
+# class Child(Parent)
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
+
+
+class Mouse(Animal):
+    def speak(self):
+        print("SQUEEK!")
+
+
+# Create objects
+dog = Dog("Buddy")
+cat = Cat("Whiskers")
+mouse = Mouse("Jerry")
+
+# Use inherited methods
+# cat.sleep()
+# mouse.eat()
+
+
+print(dog.name)
+print(dog.is_alive)
+dog.eat()
+dog.sleep()
+dog.speak()
+
+print(cat.name)
+print(mouse.name)
