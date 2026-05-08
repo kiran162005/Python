33fed176c1db9a675a21ceb007ec4d7693a76e23
kiran162005 <Kirantraj16@gmail.com>
2026-05-08 23:31:02 +0530

    Multiple Inheritance

--- 46.py
+++ 46.py
@@ -2,3 +2,8 @@
 #                        C(A,B)
 # multilevel inheritance = inherit from a parent which inherits from another parent 
 #                           C(B) <- B(A) <- A
+
+class Animal:
+
+    def __init__(self, name):
+        self.name = name
