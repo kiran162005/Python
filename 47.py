d3d9970d576f20f51c4c53fe0988285c54e21780
kiran162005 <Kirantraj16@gmail.com>
2026-05-09 23:09:21 +0530

    Super()

---
+++ 47.py
@@ -0,0 +1,40 @@
+# super() = Function used in a child class to call methods from a parent class (superclass)
+# Allow you to extend the functionality of the inherited methods
+
+
+class Shape:
+    def __init__(self,color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+    def describe(self):
+        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+    def describe(self):
+        print(f"it is a circle with an area of {3.14 * self.radius * self.radius}")
+        super().describe()
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+circle = Circle("red", True, 5)
+square = Square(color = "blue", is_filled = False, width = 6)
+triangle = Triangle(color= "yellow", is_filled=True, width=7, height=8)
+
+print(circle.color, circle.is_filled, circle.radius)
+print(square.color, square.is_filled, square.width)
+triangle.describe()
+circle.describe()
