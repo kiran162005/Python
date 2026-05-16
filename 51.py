8c7849a0d0ce0731f59922f40eab852b3e680c06
kiran162005 <Kirantraj16@gmail.com>
2026-05-16 22:45:31 +0530

    Class methods

---
+++ 51.py
@@ -0,0 +1,45 @@
+# Class methods = Allow operations related to the class itself
+# Take (cls) as the first parameter, which represents the class itself.
+
+# Instance methods = Best for operations on instances of the class (objects)
+# Static methods   = Best for utility functions that do not need access to class data
+# Class methods    = Best for class-level data or require access to the class itself
+
+
+class Student:
+    count = 0
+    total_gpa = 0
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1
+        Student.total_gpa += gpa
+
+    # INSTANCE METHOD
+    def get_info(self):
+        return f"{self.name} {self.gpa}"
+
+    # CLASS METHOD
+    @classmethod
+    def get_count(cls):
+        return f"Total # of students: {cls.count}"
+    
+    @classmethod
+    def get_average_gpa(cls):
+        if cls.count == 0:
+            return 0
+        else:
+            return f"{cls.total_gpa / cls.count:.2f}"
+
+
+# Create objects
+student1 = Student("Spongebob", 3.5)
+student2 = Student("Patrick", 2.8)
+student3 = Student("Sandy", 4.0)
+
+print(student1.get_info())
+print(student2.get_info())
+
+print("Total students:", Student.get_count())
+print(Student.get_average_gpa())
