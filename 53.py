e504966fed20b3be8159a470d3033663284dae1f
kiran162005 <Kirantraj16@gmail.com>
2026-05-21 22:48:04 +0530

    Property

--- 53.py
+++ 53.py
@@ -25,3 +25,10 @@ class Rectangle:
         else:
             print("Width must be greater than zero")
 
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Hight must be greater than zero")
+
