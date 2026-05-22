3a4ec28a515e09ebdb75a13694b9e7699656ebf9
kiran162005 <Kirantraj16@gmail.com>
2026-05-22 22:06:27 +0530

    Decorator

--- 54.py
+++ 54.py
@@ -2,25 +2,3 @@
 # w/o modifying the base function
 # Pass the base function as an argument| to the decorator
 
-# @add_sprinkles
-# get_ice_cream("vanilla")
-
-def add_sprinkles(func):
-    def wrapper(*args, **kwargs):
-        print("*You add sprinkles*")
-        func(*args, **kwargs)
-    return wrapper
-
-def add_fudge(func):
-    def wrapper(*args, **kwargs):
-        print("You add fudge")
-        func(*args, **kwargs)
-    return wrapper
-
-
-@add_sprinkles
-@add_fudge
-def get_ice_cream(flavour):
-    print("Here is your ice cream")
-
-get_ice_cream("vanilla")
