3611c592d2e6fe4aee47a91f04abae342908f478
kiran162005 <Kirantraj16@gmail.com>
2026-05-26 22:31:37 +0530

    Files

--- 57.py
+++ 57.py
@@ -42,3 +42,13 @@ employee = {
     "job": "cook"
 }
 
+file_path = "output.json"
+
+try:
+    with open(file_path, "w") as file:
+        json.dump(employee, file, indent=4)
+        print(f"json file '{file_path} was created")
+except:
+    print("That file already exists")
+'''
+
