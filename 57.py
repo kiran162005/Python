5051c93afd4fbdb9a1f0bfc053c120d8a17413d0
kiran162005 <Kirantraj16@gmail.com>
2026-05-26 22:30:43 +0530

    Files

--- 57.py
+++ 57.py
@@ -3,3 +3,10 @@
 # txt_data = "I like pizza!"
 # employees = ["Alice", "Bob", "SAndy", "Sunny"]
 
+# file_path = "output.txt"
+
+'''
+with open(file_path, "w") as file:
+    file.write(txt_data)
+    print(f"txt file '{file_path} was created")
+'''
