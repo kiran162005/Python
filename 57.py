1a56ddf621c774bce3c49ba93ace3ae095c76fd4
kiran162005 <Kirantraj16@gmail.com>
2026-05-26 22:30:52 +0530

    Files

--- 57.py
+++ 57.py
@@ -10,3 +10,10 @@ with open(file_path, "w") as file:
     file.write(txt_data)
     print(f"txt file '{file_path} was created")
 '''
+
+# try:
+#     with open(file_path, "x") as file:
+#         file.write(txt_data)
+#         print(f"txt file '{file_path} was created")
+# except:
+#     print("That file already exists")
