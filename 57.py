7de0086d0da22b23a70571e781f28e0135b4ec09
kiran162005 <Kirantraj16@gmail.com>
2026-05-26 22:31:26 +0530

    Files

--- 57.py
+++ 57.py
@@ -24,3 +24,21 @@ with open(file_path, "a") as file:
     print(f"txt file '{file_path} was created")
 '''
 
+# try:
+#     with open(file_path, "w") as file:
+#         for employee in employees:
+
+#             file.write(employee + "\n")
+#         print(f"txt file '{file_path} was created")
+# except:
+#     print("That file already exists")
+
+'''
+import json
+
+employee = {
+    "name" : "Spongebob",
+    "age" : 30,
+    "job": "cook"
+}
+
