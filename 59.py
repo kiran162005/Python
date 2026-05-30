0cb45898633ddbdc307b69ea26833e48cb1bd4e2
kiran162005 <Kirantraj16@gmail.com>
2026-05-30 21:05:38 +0530

    Date Time

--- 59.py
+++ 59.py
@@ -15,3 +15,5 @@ print(date)
 target_datetime = datetime.datetime(2030, 1, 2, 12, 30, 1)
 current_datetime = datetime.datetime.now()
 
+if target_datetime < current_datetime:
+    print("target date has passes")
