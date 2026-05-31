5e27de32f7b494f6e011cf21a41e54b91b0fdfc4
kiran162005 <Kirantraj16@gmail.com>
2026-05-31 22:11:54 +0530

    Python Alarm clock

--- 60.py
+++ 60.py
@@ -4,3 +4,19 @@ import time
 import datetime
 import pygame
 
+def set_alarm(alarm_time):
+    print(f"Alaram set for {alarm_time}")
+    sound_file = "my_music.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("Wake up !!")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
