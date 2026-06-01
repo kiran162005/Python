ebe107b4e19d180b1d79c3a5d0fc8ebeb5c5e201
kiran162005 <Kirantraj16@gmail.com>
2026-06-01 22:03:25 +0530

    Multithreading

---
+++ 61.py
@@ -0,0 +1,4 @@
+# multithreading = Used to perform multiple tasks concurrently (multitasking)
+# Good for I/O bound tasks like reading files or fetching data from APIs
+# threading.Thread(target = my_function)
+
