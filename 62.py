695579f33dff4e1cbb52ea56b8743e5c6153624c
kiran162005 <Kirantraj16@gmail.com>
2026-06-03 21:42:40 +0530

    Connecting to  API using Python

--- 62.py
+++ 62.py
@@ -4,3 +4,8 @@ import requests
 
 base_url = "https://pokeapi.co/api/v2"
 
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+    print(response)
+
