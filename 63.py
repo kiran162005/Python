add46bef8ca8156f96c130d1890411d6183b068a
kiran162005 <Kirantraj16@gmail.com>
2026-06-05 23:11:50 +0530

    PyQt5 Introduction

--- 63.py
+++ 63.py
@@ -1,20 +1,3 @@
 # PyQt5 Introduction
 # pip install PyQt5
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("GUI")
-        self.setGeometry(700, 300, 600, 500)
-
-def main():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
