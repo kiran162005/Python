c83bb14d4f18cc62acb5286328819ceb4aec1f17
kiran162005 <Kirantraj16@gmail.com>
2026-06-08 22:30:48 +0530

    PyQt5 labels

--- 64.py
+++ 64.py
@@ -34,3 +34,11 @@ class MainWindow(QMainWindow):
 
 
 
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
