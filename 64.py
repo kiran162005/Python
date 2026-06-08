311a58ad328ddd84ca3fa42364afe9f57031de1c
kiran162005 <Kirantraj16@gmail.com>
2026-06-08 22:26:27 +0530

    PyQt5 labels

--- 64.py
+++ 64.py
@@ -6,3 +6,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 600, 500)
+
