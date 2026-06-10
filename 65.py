7b40a3b94de1056f9dee7e8884bf86e34f4d25b3
kiran162005 <Kirantraj16@gmail.com>
2026-06-10 22:55:15 +0530

    PyQt Images

--- 65.py
+++ 65.py
@@ -4,3 +4,8 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+
