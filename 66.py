4523aa0d04712788b4c834dc7cb5d01bbba09e5c
kiran162005 <Kirantraj16@gmail.com>
2026-06-11 22:42:30 +0530

    PyQt5 label managers

--- 66.py
+++ 66.py
@@ -4,3 +4,9 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
                              QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.initUI()
+
