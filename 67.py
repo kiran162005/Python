b9a9b5a50e7896cb4f73fd39f77939d33b90dca7
kiran162005 <Kirantraj16@gmail.com>
2026-06-12 23:10:29 +0530

    PyQt5 buttons

--- 67.py
+++ 67.py
@@ -3,3 +3,10 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.button = QPushButton("Click me!", self)
+        self.label = QLabel("Hello", self)
+        self.initUI()
