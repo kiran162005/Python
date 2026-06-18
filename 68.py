1325b30f3a59c73932471980f24f6a04ee1afedc
kiran162005 <Kirantraj16@gmail.com>
2026-06-18 22:44:35 +0530

    Checkbox

--- 68.py
+++ 68.py
@@ -2,3 +2,7 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QCheckBox
 from PyQt5.QtCore import Qt
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
