24d89e6ac0f2e2a24c511ad0f013f28a1f360c76
kiran162005 <Kirantraj16@gmail.com>
2026-06-18 22:44:49 +0530

    Checkbox

--- 68.py
+++ 68.py
@@ -6,3 +6,20 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(700, 300, 500, 500)
+
+        self.checkbox = QCheckBox("Do you like food?", self)
+
+        self.initUI()
+
+    def initUI(self):
+        self.checkbox.setGeometry(10, 0, 500, 100)
+
+        self.checkbox.setStyleSheet("""
+            font-size: 30px;
+            font-family: Arial;
+        """)
+
+        self.checkbox.setChecked(False)
+
+        self.checkbox.stateChanged.connect(self.checkbox_changed)
+
