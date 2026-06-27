fa99bb9d4624514145cfd635b78e856366ad15bd
kiran162005 <Kirantraj16@gmail.com>
2026-06-27 23:42:15 +0530

    PyQt5 radio Buttons

--- 69.py
+++ 69.py
@@ -5,3 +5,9 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QRadioButton, QButtonGrou
 from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.radio1 = QRadioButton("Visa", self)
+        self.radio2 = QRadioButton("Mastercard", self)
+        self.radio3 = QRadioButton("Gift Card", self)
