2f1b21323236a090749478358366355d86ef1c61
kiran162005 <Kirantraj16@gmail.com>
2026-07-01 22:36:45 +0530

    PyQt5 LineEdit

--- 70.py
+++ 70.py
@@ -2,3 +2,10 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.line_edit = QLineEdit(self)
+        
